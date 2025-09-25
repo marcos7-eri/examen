8644fffecee45084dcf4b2056c3a58d314a22614
marcos7-eri <choqueerick249@gmail.com>
2025-09-25 19:40:17 -0400

    creacion de controladores para los cruds

--- PrimerParcial/Program.cs
+++ PrimerParcial/Program.cs
@@ -1,20 +1,18 @@
 using Microsoft.EntityFrameworkCore;
+using PrimerParcial.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<PrimerParcial.Data.RecetasDBContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Default_Connection")));
+builder.Services.AddDbContext<RecetasDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -27,6 +25,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Recipes}/{action=Index}/{id?}");
 
 app.Run();
